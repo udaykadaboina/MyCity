ba22adac852ae4db634b5559ac94eaf4ca7f9d28
Uday <uday88k@gmail.com>
2012-11-24 20:06:20 -0700

    add image capability before saving

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,7 @@
 class Issue < ActiveRecord::Base
-  attr_accessible :category, :description, :title, :image
   mount_uploader :image, ImageUploader
+  attr_accessible :category, :description, :title, :image
+  
   has_many :comments
   def self.categories
    [["public art","public art"], ["ABQ Parks","ABQ Parks"]].freeze
