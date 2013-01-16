edf598552b3be7427f8a425ae4dc1b2554ea55c8
Uday <uday88k@gmail.com>
2013-01-15 22:44:50 -0700

    prettying, code indentation

--- app/assets/javascripts/mashup.js
+++ app/assets/javascripts/mashup.js
@@ -1,12 +1,11 @@
 var map;
 
 function show() {  				
-				initialize();
-				for (i=0;i < total_data_sets;i++) {
-					var value = new google.maps.KmlLayer(stuff[i]);
-					value.setMap(null);
-					if ($('#'+i).is(":checked"))
-						value.setMap(map); 									
-				}
-
+	initialize();
+	for (i=0;i < total_data_sets;i++) {
+		var value = new google.maps.KmlLayer(stuff[i]);
+		value.setMap(null);
+		if ($('#'+i).is(":checked"))
+			value.setMap(map); 									
+	}
 }
--- app/assets/javascripts/myloc.js
+++ app/assets/javascripts/myloc.js
@@ -1,17 +1,18 @@
 window.onload = getMyLocation;
+
 function getMyLocation() {
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(displayLocation);
-} else {
-	alert("Oops! no geolocation support");
-}
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayLocation);
+	} else {
+		alert("Oops! no geolocation support");
+	}	
 }
 
 function displayLocation(position) {
-var latitude = position.coords.latitude; 
-var longitude = position.coords.longitude;
-var div = document.getElementById("location");
-//div.innerHTML = "Your current location is " + latitude + "," + longitude;
-document.getElementById("issue_latitude").value = latitude;
-document.getElementById("issue_longitude").value = longitude;
+	var latitude = position.coords.latitude; 
+	var longitude = position.coords.longitude;
+	var div = document.getElementById("location");
+	//div.innerHTML = "Your current location is " + latitude + "," + longitude;
+	document.getElementById("issue_latitude").value = latitude;
+	document.getElementById("issue_longitude").value = longitude;
 }
